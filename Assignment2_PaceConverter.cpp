f77ead74dd1ccd155017605625805149e087f3a5
DValdovinos4 <domanikvaldovinos@gmail.com>
2026-02-07 20:54:38 -0800

    Update Assignment2_PaceConverter.cpp

--- Assignment2_PaceConverter.cpp
+++ Assignment2_PaceConverter.cpp
@@ -1,16 +1,22 @@
 #include <iostream>
 #include <iomanip>
+#include "test.h"
 using namespace std;
 int main()
 {
     double mph = 6.5;
     // PACE calculation
-    /* write your code */
+    double pace = 60 / mph; // pace in minnutes per mile 
 
     //express pace in minutes and seconds
-  /* write your code here */
+  int minutes = static_cast<int>(pace);
 
   //express your output in 1 decimal points
-
-
+  return NewFunction(minutes, pace);
+}
+int NewFunction(int minutes, double pace)
+{
+  cout << fixed << setprecision(1);
+  cout << "Your pace is:" << minutes << " minutes and " << (pace - minutes) * 60 << " seconds per mile." << endl;
+  return 0;
 }
