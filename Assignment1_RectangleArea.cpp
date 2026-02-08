27dab55ea091819c5fceebb775ad71db48325922
DValdovinos4 <domanikvaldovinos@gmail.com>
2026-02-07 16:21:29 -0800

    Update Assignment1_RectangleArea.cpp

--- Assignment1_RectangleArea.cpp
+++ Assignment1_RectangleArea.cpp
@@ -1,24 +1,24 @@
+
 #include <iostream>
 
 using namespace std;
 
 int main()
 {
-	// print name of program
-	cout << "Rectangle Calculator" << endl << endl;
+	// Area of Rectangle 
+	cout << "Rectangle Calculator"" << endl << endl;
 
 	// get height and width from user
 	double height;
 	double width;
 	cout << "Enter height and width: ";
-/* write your code here */
+    cin >> "height" >> width;
 	// calculate area
-	
+	double area = height * width;
 
 	// write output to console
-	
+	cout << "Area" << area << endl;
 
 	// return value that indicates normal program exit
     return 0;
 }
-
