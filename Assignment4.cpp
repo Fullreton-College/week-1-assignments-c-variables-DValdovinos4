1d2a7efac9ab2dca1443785222e82cbbd7c7c70f
DValdovinos4 <domanikvaldovinos@gmail.com>
2026-02-09 12:27:03 -0800

    Update Assignment4.cpp

--- Assignment4.cpp
+++ Assignment4.cpp
@@ -1,22 +1,33 @@
 #include <iostream>
-#include <iomanip>
+
+using namespace std;
 
 int main() {
+
     int num1, num2, num3, num4;
-    std::cout << "Enter four integers:";
-    std::cin >> num1 >> num2 >> num3 >> num4;
 
-    //calculate the product and average of the four integers
-    int product_int = num1 * num2 * num3 * num4;
-    int average_int = (num1 + num2 + num3 + num4) / 4.0;
-    std::cout << "product_int: " << product_int << " average_int: " << average_int << std::endl;
+    cout << "Enter the first number:";
+    cin >> num1;
 
-    double product_float = static_cast<double>(num1) * num2 * num3 * num4;
-    double average_float = static_cast<double>(num1 + num2 + num3 + num4) / 4.0;
+    cout << "Enter the second number:";
+    cin >> num2;
 
-    std::cout << std::fixed << std::setprecision(3);
-    std::cout << product_float << " " << average_float << std::endl;
-    
-return 0;
+    cout << "Enter the third number:";
+    cin >> num3;
 
+    cout << "Enter the fourth number:";
+    cin >> num4;
+
+    cout << "You have entered " << num1 << ", " << num2 << ", " << num3 << ", and " << num4 << endl;
+
+    int product = num1 * num2 * num3 * num4;
+    cout << "The product is: " << product << endl;
+
+    int avg = (num1 + num2 + num3 + num4) / 4;
+    cout << "The average is: " << avg << endl;
+
+    double avgDouble = (double) (num1 + num2 + num3 + num4) / (double) 4;
+    cout << "Average in double is: " << avgDouble << endl;
+
+    return 0;
 }
